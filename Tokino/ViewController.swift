3ba5f81ec2ac35e92e4e1e7957b7dc86e4f07ac6
Jachimike Onuoha <jachiuzo@gmail.com>
2019-07-05 13:47:24 -0700

    Initial Commit

---
+++ Tokino/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tokino
+//
+//  Created by Jachimike Onuoha on 7/5/19.
+//  Copyright © 2019 Jachimike Onuoha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
